813c9baa596caa1663aa4067f3e65cd71bf893e5
Nelson Parreira <nizicast@gmail.com>
2022-04-23 19:18:46 +0200

    Token function added

--- utils.php
+++ utils.php
@@ -29,12 +29,37 @@
  * @param [string] $var
  * @return [string] $varName <> Returns the variable name
  ***************************************************************************/
-    function getVariavleName($var) {
-        foreach($GLOBALS as $varName => $value) {
-            if ($value === $var) {
-                return $varName;
+        function getVariavleName($var) {
+            foreach($GLOBALS as $varName => $value) {
+                if ($value === $var) {
+                    return $varName;
+                }
             }
-        }
-        return;
-    }// end function
+            return;
+        }// end function
   
+
+/**
+ * GENERATE a random TOKEN
+ *
+ * Based on a secret Kez we generate a token 
+ *
+ * //usage
+ *   $myRandomString = generateRandomString(5);
+ *   $myRandomString = generateRandomString(5, "aqwerdfgyuhjlo512397sdfmncvl48"); 
+ *
+ * @param integer $length  Specify the size of the token
+ * @return string $randomString  The token generated
+ ***************************************************************************/
+        function generateToken( $length = 25,
+                                $secretKey = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';) {
+        
+            $secretKeyLength = strlen($secretKey);
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $secretKey[rand(0, $secretKeyLength - 1)];
+            }
+            return $randomString;
+        }
+
+
